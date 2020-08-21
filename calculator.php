2a07aba4d956d42e38ac63836ed1e2bdadc40ac7
Priyanshu Verma <i.priyanshuverma@gmail.com>
2020-08-21 18:57:25 +0530

    calculator.php

---
+++ calculator.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Calculator</title>
+
+    <style>
+        h1 {
+            color: burlywood;
+            text-align: center;
+        }
+
+        #container {
+            height: 20rem;
+            width: 20rem;
+            text-align: center;
+            background-color: lightcyan;
+            color: #;
+        }
+
+        table{
+            height: 20rem;
+            width: 20rem;
+        }
+        
+    </style>
+
+<body>
+    </head>
+
+    <h1>Calculator</h1>
+
+    <div id="container">
+        <table border="10">
+
+            <tr>
+                <td id="box">
+                    <h1></h1>
+                </td>
+            </tr>
+
+            <tr->
+                <td><button>+</button></td>
+                <td><button>-</button></td>
+                <td><button>*</button></td>
+                <td><button>/</button></td>
+            </tr->
+
+            <tr>
+                <td><button>7</button></td>
+                <td><button>8</button></td>
+                <td><button>9</button></td>
+                <td><button>DEL</button></td>               
+            </tr>
+
+            <tr>
+                <td><button>4</button></td>
+                <td><button>5</button></td>
+                <td><button>6</button></td>
+                <td><button>CE</button></td>
+            </tr>
+
+            <tr>
+                <td><button>1</button></td>
+                <td><button>2</button></td>
+                <td><button>3</button></td>
+                <td><button>.</button></td>
+            </tr>
+
+            <tr>
+                <td colspan="2">0</td>
+                <td colspan="2">=</td>
+            </tr>
+
+        </table>
+    </div>
+
+
+</body>
+
+</html>
